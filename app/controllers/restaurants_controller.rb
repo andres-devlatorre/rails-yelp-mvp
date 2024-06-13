350b472922bf64e22e7ec05dbaac2029512c0f87
Andres delaTorre <andresdevlatorre@gmail.com>
2024-06-13 08:27:40 -0500

    todavia falta

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,10 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :set_restaurant, only: %i[show edit update destroy]
   def index
     @restaurants = Restaurant.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @restaurant = Restaurant.new
@@ -17,8 +16,7 @@ class RestaurantsController < ApplicationController
     redirect_to restaurant_path(@restaurant)
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @restaurant.update(restaurant_params)
